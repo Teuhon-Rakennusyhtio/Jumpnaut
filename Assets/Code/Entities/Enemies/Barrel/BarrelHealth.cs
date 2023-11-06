7a8e9872d2cb13069a26e2ad249489399a1d0931
JuTeu <juho.teulahti@gmail.com>
2023-11-06 13:15:36 +0200

    Added health pickups

--- Assets/Code/Entities/Enemies/Barrel/BarrelHealth.cs
+++ Assets/Code/Entities/Enemies/Barrel/BarrelHealth.cs
@@ -7,17 +7,6 @@ public class BarrelHealth : GenericHealth
 {
     [SerializeField] UnityEvent _die;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    protected override void DamagedLogic(Weapon weapon)
-    {
-        Debug.Log(_health);
-    }
-
     protected override void Die()
     {
         _die.Invoke();
