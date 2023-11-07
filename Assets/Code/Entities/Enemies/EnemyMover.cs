c2e6f0abbf085338974d511d9081ffbd85ca8da8
JuTeu <juho.teulahti@gmail.com>
2023-11-07 12:48:40 +0200

    Added very good AI to the enemy robot

--- Assets/Code/Entities/Enemies/EnemyMover.cs
+++ Assets/Code/Entities/Enemies/EnemyMover.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class EnemyMover : GenericMover
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void GetInputs()
     {
-        
-    }
+        //_jumpInput = Device.GetInputs[(int) ChildDeviceManager.InputTypes.jump];
+        //_useInput = Device.GetInputs[(int) ChildDeviceManager.InputTypes.use];
+        //_catchInput = Device.GetInputs[(int) ChildDeviceManager.InputTypes.pickup];
+        //_pauseInput = Device.GetInputs[(int) ChildDeviceManager.InputTypes.pause];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _catchInput = true;
+        _moveInput = Vector2.right * Random.Range(-1f, 1f);
     }
 }
