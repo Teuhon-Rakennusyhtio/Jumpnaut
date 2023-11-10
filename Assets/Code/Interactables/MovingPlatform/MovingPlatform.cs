1816d63811dab4cdf3c0764c9e33b8a197c27bb2
Juho Teulahti <juho.teulahti@wpk.tpu.fi>
2023-11-10 12:32:01 +0200

    Added more complex behaviours to moving platforms

--- Assets/Code/Interactables/MovingPlatform/MovingPlatform.cs
+++ Assets/Code/Interactables/MovingPlatform/MovingPlatform.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MovingPlatformPathTypes
+{
+    GoDirectlyToStartPoint,
+    BacktrackToStartPoint
+}
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] bool _movesOnItsOwn = false;
     [SerializeField] float _movingOnOwnDelay = 2f;
+    [SerializeField] MovingPlatformPathTypes _whenTheLastPointIsReached = MovingPlatformPathTypes.GoDirectlyToStartPoint;
+    [SerializeField] bool _waitAtEveryPathPoint = false;
+    [SerializeField] Transform[] _pathPoints;
     public float Speed = 5f;
     float _progress, _target;
     float _direction = 0f, _oldDirection = 1f;
+    int _currentStartPoint = 0;
     Transform _startPoint, _endPoint, _platfromTransform;
     Rigidbody2D _platfrom;
     Vector2 _pathVector;
@@ -18,10 +27,70 @@ public class MovingPlatform : MonoBehaviour
     {
         if (_movesOnItsOwn) _direction = 1f;
         _attachedRigidbodies = new List<Rigidbody2D>();
-        _startPoint = transform.Find("StartPoint");
-        _endPoint = transform.Find("EndPoint");
         _platfromTransform = transform.Find("Platform");
+        if (_pathPoints.Length >= 2)
+        {
+            _startPoint = _pathPoints[0];
+            _endPoint = _pathPoints[1];
+        }
+        else
+        {
+            Debug.LogError($"The moving platform \"{gameObject.name}\" does not have enough path points function!");
+            _startPoint = _platfromTransform;
+            _endPoint = _platfromTransform;
+        }
         _platfrom = _platfromTransform.gameObject.GetComponent<Rigidbody2D>();
+        _pathVector = _endPoint.position - _startPoint.position;
+        _pathVector = _pathVector.normalized;
+    }
+
+    void GetNewTargetPoints(float direction)
+    {
+        if (_pathPoints.Length == 0) return;
+        if (direction > 0f)
+        {
+            if (_currentStartPoint + 1 == _pathPoints.Length)
+            {
+                if (_whenTheLastPointIsReached == MovingPlatformPathTypes.GoDirectlyToStartPoint)
+                {
+                    _currentStartPoint = 0;
+                }
+                else
+                {
+                    _direction = 0f;
+                    return;
+                }
+            }
+            else
+            {
+                _currentStartPoint++;
+            }
+        }
+        else
+        {
+            if (_currentStartPoint == 0)
+            {
+                if (_whenTheLastPointIsReached == MovingPlatformPathTypes.GoDirectlyToStartPoint)
+                {
+                    _currentStartPoint = _pathPoints.Length - 1;
+                }
+                else
+                {
+                    _direction = 0f;
+                    return;
+                }
+            }
+            else
+            {
+                _currentStartPoint--;
+            }
+        }
+        int newEndPointIndex = _currentStartPoint + 1;
+        if (newEndPointIndex >= _pathPoints.Length)
+            newEndPointIndex = 0;
+
+        _startPoint = _pathPoints[_currentStartPoint];
+        _endPoint = _pathPoints[newEndPointIndex];
         _pathVector = _endPoint.position - _startPoint.position;
         _pathVector = _pathVector.normalized;
     }
@@ -43,18 +112,50 @@ public class MovingPlatform : MonoBehaviour
         //_progress = Mathf.MoveTowards(_progress, _target, Speed * Time.fixedDeltaTime);
         if ((_platfrom.position - (Vector2)_endPoint.position).sqrMagnitude < 0.01f && _direction == 1f)
         {
-            _direction = 0f;
-            if (_movesOnItsOwn)
+            bool backTrackNow = (_currentStartPoint == _pathPoints.Length - 2 && _whenTheLastPointIsReached == MovingPlatformPathTypes.BacktrackToStartPoint);
+            if (_waitAtEveryPathPoint || backTrackNow)
             {
-                StartCoroutine(StartMovingOnOwnWithDelay(-1f, _movingOnOwnDelay));
+                _direction = 0f;
+                if (_movesOnItsOwn && backTrackNow)
+                {
+                    StartCoroutine(StartMovingOnOwnWithDelay(-1f, _movingOnOwnDelay));
+                }
+                else
+                {
+                    GetNewTargetPoints(1f);
+                    StartCoroutine(StartMovingOnOwnWithDelay(1f, _movingOnOwnDelay));
+                }
+            }
+            else
+            {
+                GetNewTargetPoints(1f);
             }
         }
         else if ((_platfrom.position - (Vector2)_startPoint.position).sqrMagnitude < 0.01f && _direction == -1f)
         {
-            _direction = 0f;
+            /*_direction = 0f;
             if (_movesOnItsOwn)
             {
                 StartCoroutine(StartMovingOnOwnWithDelay(1f, _movingOnOwnDelay));
+            }*/
+
+            bool backTrackNow = (_currentStartPoint == 0 && _whenTheLastPointIsReached == MovingPlatformPathTypes.BacktrackToStartPoint);
+            if (_waitAtEveryPathPoint || backTrackNow)
+            {
+                _direction = 0f;
+                if (_movesOnItsOwn && backTrackNow)
+                {
+                    StartCoroutine(StartMovingOnOwnWithDelay(1f, _movingOnOwnDelay));
+                }
+                else
+                {
+                    GetNewTargetPoints(-1f);
+                    StartCoroutine(StartMovingOnOwnWithDelay(-1f, _movingOnOwnDelay));
+                }
+            }
+            else
+            {
+                GetNewTargetPoints(-1f);
             }
         }
         Vector2 movementVector = _pathVector * Speed * _direction;
