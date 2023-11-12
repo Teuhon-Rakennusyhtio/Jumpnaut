15e2e3defcee45601fd03ac15cc1eadd653c760f
JuTeu <juho.teulahti@gmail.com>
2023-11-12 15:01:01 +0200

    Added 'One Time Use' tick to the GenericTriggerVolume

--- Assets/Code/GenericTriggerVolume.cs
+++ Assets/Code/GenericTriggerVolume.cs
@@ -6,19 +6,23 @@ using UnityEngine.Events;
 public class GenericTriggerVolume : MonoBehaviour
 {
     [SerializeField] UnityEvent _enterTrigger, _exitTrigger;
+    [SerializeField] bool _oneTimeUse;
+    bool _entered, _exited;
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
+        if (collider.CompareTag("Player") && !(_oneTimeUse && _entered))
         {
             _enterTrigger.Invoke();
+            _entered = true;
         }
     }
 
     void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
+        if (collider.CompareTag("Player") && !(_oneTimeUse && _exited))
         {
             _exitTrigger.Invoke();
+            _exited = true;
         }
     }
 }
