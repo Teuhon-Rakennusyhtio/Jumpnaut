7c6f911cd56ad5593c67d27bb5a810111de748e1
Alex Yli-Paavalniemi <alex.yli-paavalniemi@tuni.fi>
2023-11-17 13:02:13 +0200

    Ny katoaa vihulaiset ja moving platformit aiemmasta alueesta kun osuu checkpointtiin. Sit viel jos piilottais sen despawnin hauskasti nii ois iha jees

---
+++ Assets/Code/Respawn/Checkpoint.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    GameObject[] enemies;
+    GameObject[] platforms;
+
+    void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        platforms = GameObject.FindGameObjectsWithTag("MovingPlatform");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            DespawnEnemies();
+            DespawnMovingPlatforms();
+        }
+    }
+
+    void DespawnEnemies()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (this.transform.position.y > enemies[i].transform.position.y)
+            {
+                enemies[i].SetActive(false);
+            }
+        }
+    }
+
+    void DespawnMovingPlatforms()
+    {
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            if (this.transform.position.y > platforms[i].transform.position.y)
+            {
+                platforms[i].SetActive(false);
+            }
+        }
+    }
+}
