85201a56979e1d0349788c2d7e30c39252ee4cd8
Alex Yli-Paavalniemi <alex.yli-paavalniemi@tuni.fi>
2023-12-06 14:20:56 +0200

    previous area locks are now a thing

--- Assets/Code/Respawn/Checkpoint.cs
+++ Assets/Code/Respawn/Checkpoint.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    GameObject spawnPoint;
     GameObject[] enemies;
     GameObject[] platforms;
+    GameObject[] previousAreaLock;
     AudioManager _audioManager;
+    private bool checkpointActivated = false;
 
     void Start()
     {
+        spawnPoint = GameObject.Find("Spawnpoint");
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
         platforms = GameObject.FindGameObjectsWithTag("MovingPlatform");
+        previousAreaLock = GameObject.FindGameObjectsWithTag("Lock");
+        for (int i = 0; i < previousAreaLock.Length; i++)
+        {
+            previousAreaLock[i].SetActive(false);
+        }
+
         GameObject audioManagerGameObject = GameObject.FindGameObjectWithTag("Audio");
         if (audioManagerGameObject != null)
             _audioManager = audioManagerGameObject.GetComponent<AudioManager>();
     }
 
+    void Update()
+    {
+        LockArea();
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Player")
+        if (!checkpointActivated && col.gameObject.tag == "Player")
         {
             _audioManager?.PlaySFX(_audioManager.checkpoint);
             if (gameObject.name != "Checkpoint 3.5" || gameObject.name != "Checkpoint 4")
@@ -27,6 +42,7 @@ public class Checkpoint : MonoBehaviour
                 DespawnEnemies();
                 DespawnMovingPlatforms();
             }
+            checkpointActivated = true;
             GetComponent<Collider2D>().enabled = false;
         }
     }
@@ -49,6 +65,17 @@ public class Checkpoint : MonoBehaviour
             if (platforms[i] != null && platforms[i].activeSelf && this.transform.position.y > platforms[i].transform.position.y + 15f)
             {
                 platforms[i].SetActive(false);
+            }
+        }
+    }
+
+    void LockArea()
+    {
+        for (int i = 0; i < previousAreaLock.Length; i++)
+        {
+            if (spawnPoint.transform.position.y > previousAreaLock[i].transform.position.y)
+            {
+                previousAreaLock[i].SetActive(true);
             }
         }
     }
